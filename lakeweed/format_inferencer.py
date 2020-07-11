5d27d5058b7d915cd53f04c2be920433505343fa
TAC <tac@tac42.net>
2020-07-12 00:04:05 +0900

    Implement multi values

--- lakeweed/format_inferencer.py
+++ lakeweed/format_inferencer.py
@@ -1,7 +1,12 @@
 import json
 import csv
 import logging
+import collections
+import pandas as pd
+import math
 
+from io import StringIO
+from . import util
 
 Invalid: str = "invalid"
 Json: str = "json"             # Json single obuject
@@ -9,53 +14,83 @@ JsonLines: str = "json_lines"  # Json lines (list of json object)
 Csv: str = "csv"               # CSV with header
 
 
+def inference_format(src: str, specified_types=None, json_delimiter="__", logger=logging.getLogger("lakeweed__format_inferencer")) -> (str, list, list):
+    if specified_types is None:
+        specified_types = {}
 
-def inference_format(src: str, logger=logging.getLogger("lakeweed__format_inferencer")) -> str:
+    keys = []
+    values_list = []
 
     # Json (single object)
-    if __is_json(src, logger):
-        return Json
+    if __is_json(src, json_delimiter, logger, keys, values_list):
+        return Json, keys, values_list
 
     # Json Lines
-    if __is_multi_jsons(src, logger):
-        return JsonLines
+    if __is_multi_jsons(src, json_delimiter, logger, keys, values_list):
+        return JsonLines, keys, values_list
 
     # CSV with header
-    if __is_csv(src, logger):
-        return Csv
+    if __is_csv(src, logger, keys, values_list):
+        return Csv, keys, values_list
 
-    return Invalid
+    return Invalid, [], []
 
 
-def __is_json(src, logger) -> bool:
+def __is_json(src, delimiter, logger, keys: list, values_list: list) -> bool:
     try:
-        json.loads(src)
+        body = json.loads(src)
+        flatten_body = util.flatten(body, delimiter=delimiter)
+        values = []
+        for k, v in flatten_body.items():
+            keys.append(k)
+            values.append(v)
+
+        values_list.append(values)
+
         return True
+
     except json.JSONDecodeError:
         logger.debug(f"{src}: is not JSON")
         return False
 
 
-def __is_multi_jsons(src, logger) -> bool:
+def __is_multi_jsons(src, delimiter, logger, keys: list, values_list: list) -> bool:
     stripped_src = src.strip()
-    lines = stripped_src.split()
+    lines = stripped_src.split('\n')
     line_count = len(lines)
     if line_count <= 1:
         logger.debug(f"{src}: is not JSON multiline because row count is too few ({line_count})")
         return False
 
-    line_number = 1
+    orderd_keys = collections.OrderedDict()  # to make unique key set
     for line in lines:
-        if not __is_json(line, logger):
-            logger.debug(f"in Line Number: {line_number}")
+        tmp_keys = []
+        tmp_value = []
+        if not __is_json(line, delimiter, logger, tmp_keys, tmp_value):
+            logger.debug(f"in Line Number: {len(values_list)+1}")
             return False
 
-        line_number += 1
+        tmp_key_value = {}
+        for idx, k in enumerate(tmp_keys):
+            if k not in orderd_keys.keys():
+                orderd_keys[k] = True
+            tmp_key_value[k] = tmp_value[0][idx]  # value will be store single list into __is_json.
 
+        values_in_this_line = []
+        for k in orderd_keys:
+            v = None
+            if k in tmp_key_value.keys():
+                v = tmp_key_value[k]
+            values_in_this_line.append(v)
+
+        values_list.append(values_in_this_line)
+
+    keys.extend(orderd_keys.keys())
     # All lines are able to parse as Json !
     return True
 
-def __is_csv(src, logger) -> bool:
+
+def __is_csv(src, logger, keys: list, values_list: list) -> bool:
     stripped_src = src.strip()
     line_count = len(stripped_src.split())
 
@@ -64,8 +99,21 @@ def __is_csv(src, logger) -> bool:
         return False
 
     try:
-        return csv.Sniffer().has_header(stripped_src)
+        if not csv.Sniffer().has_header(stripped_src):
+            return False
+
+        dialect = csv.Sniffer().sniff(stripped_src)
+        dialect.skipinitialspace = True
+
+        with StringIO(stripped_src) as io:
+            df = pd.read_csv(io)
+            keys.extend(list(df.columns))
+            for line in df.values:
+                l = [None if type(v) is float and math.isnan(v) else v for v in line.tolist()]
+                values_list.append(l)
+
+        return True
+
     except ValueError as e:
-        print(e)
-        logger.debug(f"{src}: is not CSV")
+        logger.debug(f"{src}: is not CSV. {e}")
         pass
--- tests/test_format_inferencer.py
+++ tests/test_format_inferencer.py
@@ -2,38 +2,74 @@
 from lakeweed import format_inferencer as fi
 
 
+# -------------- Invalid -------------- #
 def test_inference_format_reteurn_Invalid_src_is_not_formated():
     src = """
     hello!
     """
 
-    format = fi.inference_format(src)
-    assert format == fi.Invalid
+    (format, keys, values_list) = fi.inference_format(src)
+    assert fi.Invalid == format
+    assert keys == []
+    assert values_list == []
 
 
+# -------------- Json -------------- #
 def test_inference_format_reteurn_json_src_is_sipmle_json():
     src = """
     {}
     """
 
-    format = fi.inference_format(src)
-    assert format == fi.Json
+    (format, keys, values_list) = fi.inference_format(src)
+    assert fi.Json == format
+    assert [] == keys
+    assert [[]] == values_list
 
+
+def test_inference_format_reteurn_json_obj_src_is_sipmle_json():
+    src = """
+    { "hello" : 42, "world" : 128.4, "bool" : true, "str" : "Hello,World" }
+    """
+
+    (format, keys, values_list) = fi.inference_format(src)
+    assert fi.Json == format
+    assert ['hello', 'world', 'bool', 'str'] == keys
+    assert [[42, 128.4, True, 'Hello,World']] == values_list
+
+
+# -------------- JsonLines -------------- #
 def test_inference_format_reteurn_list_of_json_src_is_multi_line_json_objects():
     src = """
     {}
     {}
     """
 
-    format = fi.inference_format(src)
-    assert format == fi.JsonLines
+    (format, keys, values_list) = fi.inference_format(src)
+    assert fi.JsonLines == format
+    assert [] == keys
+    assert [[], []] == values_list
 
 
-def test_inference_format_reteurn_csv_src_is_basic_csv():
+def test_inference_format_reteurn_list_of_json_obj_src_is_multi_line_json_objects():
     src = """
-    sample_column01,b,c,d,e
-    10,,"hello,world",,
+    { "hello" : 42, "world" : 128.4, "bool" : true, "str" : "Hello,World" }
+    { "hello" : 42, "world" : 128.4, "bool" : true, "str" : "Hello,World" }
     """
 
-    format = fi.inference_format(src)
-    assert format == fi.Csv
+    (format, keys, values_list) = fi.inference_format(src)
+    assert fi.JsonLines == format
+    assert ['hello', 'world', 'bool', 'str'] == keys
+    assert [[42, 128.4, True, 'Hello,World'], [42, 128.4, True, 'Hello,World']] == values_list
+
+# -------------- CSV -------------- #
+def test_inference_format_reteurn_csv_src_is_basic_csv():
+    src = """
+    a,b,c,d,e
+    10,,"hello,world",,
+    42,hoge,"hello,world",,
+    """
+
+    (format, keys, values_list) = fi.inference_format(src)
+    assert fi.Csv == format
+    assert ["a", "b", "c", "d", "e"] == keys
+    assert [[10, None, "hello,world", None, None], [42, 'hoge', 'hello,world', None, None]] == values_list
