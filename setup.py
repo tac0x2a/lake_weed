7c30676b13007afe473844583d929454e1a4997d
TAC <tac@tac42.net>
2020-07-12 20:58:27 +0900

    Fix required packages

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
 setup(
     name="lakeweed",
-    version="1.0.0",
+    version="1.0.1",
     author="TAC",
     author_email="tac@tac42.net",
-    description=("Elastic converter JSON to RDB record"),
+    description=("Elastic parser for data-like string, JSON, JSON Lines, and CSV to RDB record"),
     license="MIT",
-    keywords="json clickhouse",
+    keywords="json jsonline csv rabbitmq amqp clickhouse",
     url="https://github.com/tac0x2a/lake_weed",
     packages=['lakeweed'],
+    install_requires=_requires_from_file('requirements.txt')
 )
